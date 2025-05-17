c462b2dd2bbe918e59222298e80328a7276dea8b
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-05-17 15:43:03 -0300

    update: rwemoçao de varivel de cor

--- Twitter-nuxt/components/Sidebar.vue
+++ Twitter-nuxt/components/Sidebar.vue
@@ -20,7 +20,7 @@ const menuItems = [
 <template>
   <nav class="flex flex-col h-screen w-64 bg-black text-white py-6 px-2 fixed left-0 top-0 border-r border-neutral-800">
     <div class="mb-8 flex items-center px-4">
-      <Icon name="ic:baseline-home" class="text-[#1da1f2]text-3xl mr-2" />
+      <Icon name="ic:baseline-home" class="text-[#1da1f2] text-3xl mr-2" />
       <span class="font-bold text-xl">Página Inicial</span>
     </div>
     <ul class="flex-1 flex flex-col gap-2">
@@ -32,9 +32,7 @@ const menuItems = [
 </template>
 
 <style scoped>
-.text-twitter-blue {
-  color: #1da1f2;
-}
+
 @media (max-width: 1024px) {
   nav {
     width: 72px;
